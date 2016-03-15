3d50a1ef4cfaecf5b2b8e98ba26bc487ba4a3ebb
James Avakian <javakian1@gmail.com>
2016-03-15 13:13:51 -0700

    Real Estate Reporter
    Real Estate reporter is a camera with notes so realtors and appraisal
    can report on the status condition of a house.

---
+++ CD-Grocery-ImagesUITests/CD_Grocery_ImagesUITests.swift
@@ -0,0 +1,36 @@
+//
+//  CD_Grocery_ImagesUITests.swift
+//  Real Estate Reporter
+//
+//  Created by James Avakian on 3/15/2016
+//  Copyright © 2016 Optical Automation, LLC. All rights reserved.
+//
+//  Copyright © 2015 Jason Rybka. All rights reserved.
+//
+
+import XCTest
+
+class CD_Grocery_ImagesUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Item+CoreDataProperties.swift
+//  Real Estate Reporter
+//
+//  Created by James Avakian on 3/15/2016
+//  Copyright © 2016 Optical Automation, LLC. All rights reserved.
+//
+//  Delete this file and regenerate it using "Create NSManagedObject Subclass…"
+//  to keep your implementation up to date with your model.
+//
+
+import Foundation
+import CoreData
+
+extension Item {
+
+    @NSManaged var image: NSData?
+    @NSManaged var name: String?
+    @NSManaged var note: String?
+    @NSManaged var qty: String?
+
+}
